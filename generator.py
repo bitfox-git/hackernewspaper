73c1367e7197b694bc918e58add2954582f19043
DvdPlaat <159027604+DvdPlaat@users.noreply.github.com>
2024-02-14 09:14:53 +0100

    Added features & Automatic newspapers (#5)
    * feat: add download folder instead of spitting in main directory
    
    * feat: add some more icons
    
    * feat: implement base url handling
    
    * feat: combine photo scraping & html downloading in one
    
    * feat: youtube data downloader
    
    * feat: pdf data extractor
    
    * feat: make workflow check for tags
    
    * chore: add new deps
    
    * fix: bad chatgpt flow
    
    * fix: idk what github is doing
    
    * fix: try to use new workflow syntax thingy
    
    * fix: try this
    
    * feat: try this
    
    * fix: try pulling
    
    * fix: try some magic
    
    * fix: try some magic
    
    * fix: remove the $edition
    
    * fix: mailchimp not giving 404's
    
    * fix: use github output
    
    * fix: tectonic being funny
    
    * feat: skip action
    
    * fix: syntax
    
    * fix: make it a draft
    
    * fix: remove draft
    
    * feat: create schedule to workflow
    
    * fix: workflow not getting the latest tag
    
    * feat: github handler
    
    * feat: update workflow to check for latest edition
    
    * fix: edition not being put into env no more
    
    * fix: syntax again
    
    * fix: it not working on this weeks pdf
    
    * chore: update all action versions
    
    * fix: make python also use version 20 node
    
    * style: deduplicate a bit of code
    
    * style: a bit more code shortening

--- generator.py
+++ generator.py
@@ -6,57 +6,23 @@ import urllib.request
 import urllib.parse
 from trafilatura import extract, extract_metadata
 from fake_useragent import UserAgent
+from config import asset_dir,ISSUE
 
-#read the first argument from the command line and use it as the issue number
-if len(sys.argv) > 1:
-    ISSUE = sys.argv[1]
-else:
-    ISSUE = "675" 
+from url_handlers import (
+    DefaultHandler,
+    GithubHandler,
+    PDFHandler,
+    YoutubeHandler,
+    download_html,
+)
+
+os.makedirs(asset_dir, exist_ok=True)
+
+ua = UserAgent()
 
 paperdata = {
     "issue": ISSUE
 }
-
-#Some weird url workarounds....
-if ISSUE == "675":
-    ISSUE= "674-2563024"
-    
-
-ua = UserAgent()
-
-# download the html from a given url 
-def download_html(url):
-    header = {'User-Agent':str(ua.random)}
-
-    #In issue 668 there is a url with a space in it, which is not allowed, so we need to encode it.
-    url = urllib.parse.quote(url, safe='/:?=&')
-
-
-    try:
-        with urllib.request.urlopen(urllib.request.Request(url, headers=header)) as response:
-        # check if the request was successful
-            content_type = response.getheader("Content-Type")
-            if content_type is None:
-                content_type = "text/html"
-            # todo : check if the header is text/html
-            if content_type.startswith("video"):
-                html = "This is a video url."
-            else:
-                html = response.read()
-                #Check if the encoding is utf-8, otherwise convert to utf-8
-                if response.info().get_content_charset() == 'utf-8':
-                    html = html.decode("utf-8")
-                else:
-                    html = html.decode("latin-1")                    
-    except urllib.error.HTTPError as e:
-        html = "Could not download this url."
-    except urllib.error.URLError as e:
-        html = "Could not download this url."
-    except: 
-        html = "Could not download this url."
-    return html
-
-
 
 # use BeautifulSoup to parse the html version of the newsletter
 def parse_html(html):
@@ -119,27 +85,15 @@ def get_articles(soup):
                         suburla = span.find("a") 
                         if suburla is not None: 
                             suburl = suburla.get("href")
-                    art = article(mainurl, title, text, subtext, suburl,category)    
+                    art = article(mainurl, title, text, subtext, suburl, category)    
                     articles.append(art)
                     print(art)       
     return articles, categories
 
-#check if url is a youtube url using the regex ^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+
-def is_youtube_url(url):
-    regex = "^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+"
-    if re.match(regex, url):
-        return True
-    else:
-        return False
 
 
-#generate screenshot of the url using playwright
-def generate_screenshot(index, url, browser):
-    page = browser.new_page()
-    page.goto(url)
-    #TODO Solve the cookie accept problem 
-    page.screenshot(path=f'{index}.png')
-    page.close()
+
+
 
 html = download_html("https://mailchi.mp/hackernewsletter/"+ISSUE)
 soup = parse_html(html)
@@ -147,159 +101,23 @@ header = get_header(soup)
 
 articles, categories = get_articles(soup)
 
-with sync_playwright() as p:
-    browser = p.chromium.launch()
-    for index, art in enumerate(articles):
-        
-        #if file exists skip
-        if os.path.isfile(f'{index}.png') or os.path.isfile(f'{index}.jpg'):
-            continue
-
-        # todo : sometimes the url is valid, but it is not a single video url , but a playlist url
-        # in that case we should skip it
-        # for now the Thumbnail class will throw an exception, but we should catch it and skip it
-        
-        try:
-            if (is_youtube_url(art.mainurl)):    
-                # youtube stores it as JPG  
-                t = Thumbnail(art.mainurl)
-                t.fetch()
-                t.save(dir=".", filename=f'{index}', overwrite=True)
-            else:
-                generate_screenshot(index, art.mainurl, browser)
-        except:
-            #TODO : create a timeout/404 default jpg.
-            storeAFakeUrlLater=True
-    browser.close()
-
 # parse the content of each link
 newsitems = []
 
-# load or download art.mainurl contents
-def loadordownload(index, art):
-    fname = f'{index}.html'
-    if os.path.isfile(fname):
-        with open(fname, encoding="utf-8") as f:
-            sitecontent = f.read()
-    else:
-        sitecontent = download_html(art.mainurl)
-        with open(fname, "w", encoding="utf-8") as f:
-            f.write(sitecontent)
-    return sitecontent
-
-def splitFirstSentenceParagraph(data):
-    # find the indexes of all occurences of a dot, question mark or exclamation mark
-    dots = [i for i, ltr in enumerate(data) if ltr in [".", "?", "!"] and i<200]
-    # find the highest index that is smaller than 200 and make sure the max is not taken over an empty set
-    #dots2 = [i for i in dots if i < 200]
-    if (len(dots) > 0):
-        firstdot = max(dots)
-        if firstdot > 10:
-            return data[:firstdot+1], data[firstdot+1:]
-    return None, data
-
-def removeEmptyLines(data):
-    # remove empty lines
-    lines = data.split("\n")
-    lines = [line for line in lines if line.strip() != ""]
-    return "\n".join(lines)
-
-# map the site to a fontawesome symbol
-def faSymbolPerHostname(hostname):
-    match hostname:
-        case "youtube.com": return "Youtube"
-        case "flikr.com": return "Flikr"
-        case "github.com": return "Github"
-        case "medium.com": return "Medium"
-        case _ : return "Globe"
-
-def isValidDictItem(item, dict):
-    return item in dict and dict[item] is not None and dict[item] != ""
 
 
-for index, art in enumerate(articles):
-    sitecontent= loadordownload(index, art)
-    data  = extract(sitecontent)
 
-    if data is None:
-        data = ""
+handlers = [YoutubeHandler(), PDFHandler(), GithubHandler(), DefaultHandler()]
 
-    # temp remove all emoji stuff, until found decent solutions in latex
-    # solved : no longer necessary with Tectonic Typesetting.
-    # data = removeUnicode(data)
-    # data can contain a lot of characters, we only want the first 1500
-    data = data[0:min(1100, len(data))]
-    # santize the data by removing % and / 
-    data = data.replace("%", "")
-    data = data.replace("\\", "")
-
-    # remove empty lines
-    data = removeEmptyLines(data)
-    firstSentence, data = splitFirstSentenceParagraph(data)
-
-    # the string art.title might contain text and numbers  of the format "Votes: 521 Comments: 83"
-    # we want to extract the numbers
-    votes= 0
-    comments = 0
-    if (art.title is not None):
-        numbers = re.findall(r'\d+', art.title) 
-        if (len(numbers) ==2):
-            votes = int(numbers[0])
-            comments = int(numbers[1])
-
-    metadata = extract_metadata(sitecontent)
-    if metadata is None:
-        metadatadict   = {}
-    else:
-        metadatadict = metadata.as_dict()
-    
-    # we extend the metadata with the votes and comments counts if they are > 0
-    if (votes > 0):
-        metadatadict["votes"] = votes
-    if (comments > 0):
-        metadatadict["comments"] = comments
-
-    # fall back 
-    image = "notfound.png"
-    #image url can be either a png or a jpg
-    if os.path.isfile(f'{index}.png'):
-        image = f'{index}.png'
-    
-    if os.path.isfile(f'{index}.jpg'):
-        image = f'{index}.jpg'
-
-    #potential interested metadata fields are: author, date, image, sitename
-    #not used , but for future reference : 
-    # pagetype [object, article, website ]
-    # description
-    # categories and tags
-
-    newsproperties = []
-    
-    if (isValidDictItem("author", metadatadict)): 
-        newsproperties.append({ "symbol": "User", "value" : metadatadict["author"], "url": None})
-    if (isValidDictItem("date", metadatadict)):
-        newsproperties.append({ "symbol": "Calendar", "value" : metadatadict["date"], "url": None})
-    if (isValidDictItem("hostname", metadatadict)):
-        newsproperties.append({ "symbol": faSymbolPerHostname(metadatadict["hostname"]), "value" : metadatadict["hostname"], "url": None})
-    if ("votes" in metadatadict):
-        newsproperties.append({ "symbol": "ThumbsOUp", "value" : metadatadict["votes"], "url": art.suburl})
-    if ("comments" in metadatadict):
-        newsproperties.append({ "symbol": "Comments", "value" : metadatadict["comments"], "url": art.suburl})
-
-    if (is_youtube_url(art.mainurl)): 
-        #determine the length of the video as a properties
-        #first read is that you would need a youtube api key to get this information
-        skipthisForNow = True
-    
-
-    newsitems.append({"title": art.text, 
-                      "url" : art.mainurl,
-                      "image": image, 
-                      "category" : art.category,
-                      "firstline": firstSentence,
-                      "content": data, 
-                      "properties": newsproperties })
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    for index, art in enumerate(articles):
+        for handler in handlers:
+            if handler.test(art):
+                # TODO maybe make it so if it throws a exception fallback to another handler
+                newsitems.append(handler.work(index, art, browser))
+                break
+    browser.close()
 
 quoteLine, quoteAuthor = parse_header(header)
 
---
+++ url_handlers.py
@@ -0,0 +1,455 @@
+import os , sys , re
+from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright
+from pythumb import Thumbnail
+import urllib.request
+import urllib.parse
+from trafilatura import extract, extract_metadata
+from fake_useragent import UserAgent
+from config import asset_dir
+import yt_dlp
+import json
+from pypdf import PdfReader
+from PIL import Image
+
+ua = UserAgent()
+
+# download the html from a given url 
+def download_html(url):
+    header = {'User-Agent':str(ua.random)}
+
+    #In issue 668 there is a url with a space in it, which is not allowed, so we need to encode it.
+    url = urllib.parse.quote(url, safe='/:?=&')
+
+
+    try:
+        with urllib.request.urlopen(urllib.request.Request(url, headers=header)) as response:
+        # check if the request was successful
+            content_type = response.getheader("Content-Type")
+            if content_type is None:
+                content_type = "text/html"
+            # todo : check if the header is text/html
+            if content_type.startswith("video"):
+                html = "This is a video url."
+            else:
+                html = response.read()
+                #Check if the encoding is utf-8, otherwise convert to utf-8
+                if response.info().get_content_charset() == 'utf-8':
+                    html = html.decode("utf-8")
+                else:
+                    html = html.decode("latin-1")                    
+    except urllib.error.HTTPError as e:
+        html = "Could not download this url."
+    except urllib.error.URLError as e:
+        html = "Could not download this url."
+    except: 
+        html = "Could not download this url."
+    return html
+
+
+#check if url is a youtube url using the regex ^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+
+def is_youtube_url(url):
+    regex = "^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+"
+    if re.match(regex, url):
+        return True
+    else:
+        return False
+
+# load or download art.mainurl contents
+def loadordownload(index, art):
+    fname = f'{asset_dir}{index}.html'
+    if os.path.isfile(fname):
+        with open(fname, encoding="utf-8") as f:
+            sitecontent = f.read()
+    else:
+        sitecontent = download_html(art.mainurl)
+        with open(fname, "w", encoding="utf-8") as f:
+            f.write(sitecontent)
+    return sitecontent
+
+def splitFirstSentenceParagraph(data):
+    # find the indexes of all occurences of a dot, question mark or exclamation mark
+    dots = [i for i, ltr in enumerate(data) if ltr in [".", "?", "!"] and i<200]
+    # find the highest index that is smaller than 200 and make sure the max is not taken over an empty set
+    #dots2 = [i for i in dots if i < 200]
+    if (len(dots) > 0):
+        firstdot = max(dots)
+        if firstdot > 10:
+            return data[:firstdot+1], data[firstdot+1:]
+    return None, data
+
+def removeEmptyLines(data):
+    # remove empty lines
+    lines = data.split("\n")
+    lines = [line for line in lines if line.strip() != ""]
+    return "\n".join(lines)
+
+#generate screenshot of the url using playwright
+def generate_screenshot(index, url, browser):
+    page = browser.new_page()
+    page.goto(url)
+    #TODO Solve the cookie accept problem 
+    page.screenshot(path=f'{asset_dir}{index}.png')
+    page.close()
+
+class UrlHandler(): 
+    def test(self, art) -> bool:
+        return False;
+    def work(self, index, art):
+        pass
+
+def isValidDictItem(item, dict):
+    return item in dict and dict[item] is not None and dict[item] != ""
+
+# map the site to a fontawesome symbol
+# https://www.comet.com/standardizing-experiment/eda-hackernews-data/reports/standardizing-the-experiment-exploring-the-hackernews-dataset
+def faSymbolPerHostname(hostname: str):
+    match hostname:
+        case "flikr.com": return "Flikr"
+        case "github.com": return "Github"
+        case "medium.com": return "Medium"
+        case "twitter.com": return "Twitter"
+        case "nytimes.com": return "NewspaperO"
+        case "wikipedia.org": return "WikipediaW"
+        case "reddit.com": return "Reddit"
+        case "ycombinator.com": return "YCombinator"
+        # Youtube
+        case "youtube.com": return "Youtube"
+        case "youtu.be": return "Youtube"
+        # Github
+        case "github.io": return "Github"
+        case "github.com": return "Github"
+        case "github.blog": return "Github"
+        # News Papers
+        case "theguardian.com": return "NewspaperO"
+        case "dev.to": return "NewspaperO"
+        case "techcrunch.com": return "NewspaperO"
+        case "wsj.com": return "NewspaperO"
+        case "arstechnica.com": return "NewspaperO"
+        case "theverge.com": return "NewspaperO"
+        case "bbc.com": return "NewspaperO"
+        case "bloomberg.com": return "NewspaperO"
+        case "reuters.com": return "NewspaperO"
+        # Globe for others
+        case _ : return "Globe"
+
+def write(index:int, data):
+    with open(f'{asset_dir}{index}.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+def read(index:int):
+    try:
+        with open(f'{asset_dir}{index}.json') as data_file:
+            return json.load(data_file)
+    except:
+        return None
+
+def download_bin(url):
+    header = {'User-Agent':str(ua.random)}
+    try:
+        with urllib.request.urlopen(urllib.request.Request(url, headers=header)) as response:
+            return response.read()
+    except:
+        return None
+
+def cached_download(url:str, index:int, ext:str):
+    fname = f'{asset_dir}{index}.{ext}'
+    if os.path.isfile(fname):
+        return True
+    else:
+        sitecontent = download_bin(url)
+        if sitecontent is None:
+            return False
+        with open(fname, "wb") as f:
+            f.write(sitecontent)
+        return True
+
+
+
+def get_url_extension(url):
+    parsed_url = urllib.parse.urlparse(url)
+    path = parsed_url.path
+    return os.path.splitext(path)[1]
+
+
+def get_metadata(title: str, metadatadict: dict = {}) -> dict:
+    votes = 0
+    comments = 0
+    if title is not None:
+        numbers = re.findall(r"\d+", title)
+        if len(numbers) == 2:
+            votes = int(numbers[0])
+            comments = int(numbers[1])
+
+    if votes > 0:
+        metadatadict["votes"] = votes
+    if comments > 0:
+        metadatadict["comments"] = comments
+    return metadatadict
+
+
+def add_stats(props: list[dict], metadatadict: dict, link: str):
+    if "votes" in metadatadict:
+        props.append(
+            {
+                "symbol": "ThumbsOUp",
+                "value": metadatadict["votes"],
+                "url": link,
+            }
+        )
+    if "comments" in metadatadict:
+        props.append(
+            {
+                "symbol": "Comments",
+                "value": metadatadict["comments"],
+                "url": link,
+            }
+        )
+
+
+
+def is_github_repo(url):
+    pattern = r"^https?://github\.com/[\w.-]+/[\w.-]+(?:\?.*)?$"
+    return bool(re.match(pattern, url))
+
+
+def prep_body(text: str | None):
+    if text is None:
+        text = ""
+
+    text = text[0 : min(1100, len(text))]
+
+    text = (
+        text.replace("%", "")
+        .replace("\u001b", "")
+        .replace("\u000F", "")
+        .replace("\\", "")
+    )
+
+    # remove empty lines
+    text = removeEmptyLines(text)
+    firstSentence, text = splitFirstSentenceParagraph(text)
+
+    return firstSentence, text
+
+
+class YoutubeHandler():
+    def test(self, art):
+        return art.mainurl.startswith("https://www.youtube.com/watch?v=")
+
+    def work(self, index, art, browser):
+        ydl = yt_dlp.YoutubeDL()
+
+        video_info = read(index)
+        if video_info is None:
+            video_info = ydl.extract_info(art.mainurl, download=False)
+            write(index, video_info)
+
+        metadatadict = get_metadata(art.title)
+
+        firstSentence, data = prep_body(video_info["description"])
+
+        image = "notfound.png"
+        if cached_download(video_info["thumbnail"], index, "jpg"):
+            image = f"{asset_dir}{index}.jpg"
+            # tectonic was being funny with the standard youtube jpg thumbnails so we convert them to PNG and it doesnt complain anymore :)
+            im = Image.open(image)
+            image = f"{asset_dir}{index}.png"
+            im.save(image)
+
+        newsproperties = []
+
+        newsproperties.append(
+            {"symbol": "User", "value": video_info["channel"], "url": None}
+        )
+        upload = video_info["upload_date"]
+
+        newsproperties.append(
+            {
+                "symbol": "Calendar",
+                "value": f"{upload[:4]}-{upload[4:6]}-{upload[6:]}",
+                "url": None,
+            }
+        )
+
+        newsproperties.append(
+            {
+                "symbol": faSymbolPerHostname("youtube.com"),
+                "value": "youtube.com",
+                "url": None,
+            }
+        )
+
+        add_stats(newsproperties, metadatadict, art.suburl)
+
+        return {
+            "title": art.text,
+            "url": art.mainurl,
+            "image": image,
+            "category": art.category,
+            "firstline": firstSentence,
+            "content": data,
+            "properties": newsproperties,
+        }
+
+
+
+
+class GithubHandler:
+    def test(self, art):
+        return is_github_repo(art.mainurl)
+
+    def work(self, index, art, browser):
+        # TODO clean this up
+
+        sitecontent = loadordownload(index, art)
+        metadata = extract_metadata(sitecontent)
+        data = metadata.description + " " + extract(sitecontent)
+
+        firstSentence, data = prep_body(data)
+
+        metadatadict = get_metadata(
+            art.title, metadata.as_dict() if metadata is not None else {}
+        )
+
+        newsproperties = []
+
+        if isValidDictItem("author", metadatadict):
+            newsproperties.append(
+                {"symbol": "User", "value": metadatadict["author"], "url": None}
+            )
+        if isValidDictItem("date", metadatadict):
+            newsproperties.append(
+                {"symbol": "Calendar", "value": metadatadict["date"], "url": None}
+            )
+        if isValidDictItem("hostname", metadatadict):
+            newsproperties.append(
+                {
+                    "symbol": faSymbolPerHostname(metadatadict["hostname"]),
+                    "value": metadatadict["hostname"],
+                    "url": None,
+                }
+            )
+
+        add_stats(newsproperties, metadatadict, art.suburl)
+
+        cached_download(metadata.image, index, "png")
+
+        return {
+            "title": art.text,
+            "url": art.mainurl,
+            "image": f"{asset_dir}/{index}.png",
+            "category": art.category,
+            "firstline": firstSentence,
+            "content": data,
+            "properties": newsproperties,
+        }
+
+
+class PDFHandler:
+    def test(self, art):
+        # Ofcourse this check isnt perfect but shoulod catch 99% of all pdf's
+        return get_url_extension(art.mainurl) == ".pdf"
+
+    def work(self, index, art, browser):
+        # TODO: fix screenshots for pdf's they currently do not work!
+
+        metadatadict = get_metadata(art.title)
+
+        data = ""
+
+        if cached_download(art.mainurl, index, "pdf"):
+            pdf = f"{asset_dir}{index}.pdf"
+            reader = PdfReader(pdf)
+            number_of_pages = len(reader.pages)
+            page = reader.pages[0]
+            text = page.extract_text()
+            if number_of_pages != 1:
+                text += " " + reader.pages[1].extract_text()
+            data = text
+        # temp remove all emoji stuff, until found decent solutions in latex
+        # solved : no longer necessary with Tectonic Typesetting.
+        # data = removeUnicode(data)
+        # data can contain a lot of characters, we only want the first 1500
+        # santize the data by removing % and /
+
+        firstSentence, data = prep_body(data)
+
+        image = "notfound.png"
+
+        if os.path.isfile(f"{asset_dir}{index}.png"):
+            image = f"{asset_dir}{index}.png"
+
+        newsproperties = []
+
+        add_stats(newsproperties, metadatadict, art.suburl)
+
+        return {
+            "title": art.text,
+            "url": art.mainurl,
+            "image": image,
+            "category": art.category,
+            "firstline": firstSentence,
+            "content": data,
+            "properties": newsproperties,
+        }
+
+
+class DefaultHandler(UrlHandler):
+    def test(self, art):
+        return True
+    def work(self, index, art, browser):
+        # TODO clean this up
+
+        #if file exists skip
+        if not os.path.isfile(f"{asset_dir}{index}.png") and not os.path.isfile(
+            f"{asset_dir}{index}.jpg"
+        ):
+            try:
+                generate_screenshot(index, art.mainurl, browser)
+            except:  # noqa: E722
+                # TODO : create a timeout/404 default jpg.
+                pass
+
+        sitecontent = loadordownload(index, art)
+        data  = extract(sitecontent)
+
+        firstSentence, data = prep_body(data)
+
+        metadata = extract_metadata(sitecontent)
+
+        metadatadict = get_metadata(
+            art.title, metadata.as_dict() if metadata is not None else {}
+        )
+
+        # fall back 
+        image = "notfound.png"
+        #image url can be either a png or a jpg
+        if os.path.isfile(f'{asset_dir}{index}.png'):
+            image = f"{asset_dir}{index}.png"
+
+        #potential interested metadata fields are: author, date, image, sitename
+        #not used , but for future reference : 
+        # pagetype [object, article, website ]
+        # description
+        # categories and tags
+
+        newsproperties = []
+        
+        if (isValidDictItem("author", metadatadict)): 
+            newsproperties.append({ "symbol": "User", "value" : metadatadict["author"], "url": None})
+        if (isValidDictItem("date", metadatadict)):
+            newsproperties.append({ "symbol": "Calendar", "value" : metadatadict["date"], "url": None})
+        if (isValidDictItem("hostname", metadatadict)):
+            newsproperties.append({ "symbol": faSymbolPerHostname(metadatadict["hostname"]), "value" : metadatadict["hostname"], "url": None})
+
+        add_stats(newsproperties, metadatadict, art.suburl)
+
+        return {
+                "title": art.text, 
+                "url" : art.mainurl,
+                "image": image, 
+                "category" : art.category,
+                "firstline": firstSentence,
+                "content": data, 
+                "properties": newsproperties 
+            }
