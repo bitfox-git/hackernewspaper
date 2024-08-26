b01adc506cc78f851ae09e18cc0e087ef3bfd8ba
Nick <nick@pc.local>
2024-08-26 15:49:40 +0200

    reformat

--- generator.py
+++ generator.py
@@ -1,4 +1,4 @@
-import os , sys , re
+import os, sys, re
 from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 from pythumb import Thumbnail
@@ -6,7 +6,7 @@ import urllib.request
 import urllib.parse
 from trafilatura import extract, extract_metadata
 from fake_useragent import UserAgent
-from config import asset_dir,ISSUE
+from config import asset_dir, ISSUE
 
 from url_handlers import (
     DefaultHandler,
@@ -20,13 +20,12 @@ os.makedirs(asset_dir, exist_ok=True)
 
 ua = UserAgent()
 
-paperdata = {
-    "issue": ISSUE
-}
+paperdata = {"issue": ISSUE}
+
 
 # use BeautifulSoup to parse the html version of the newsletter
 def parse_html(html):
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, "html.parser")
     return soup
 
 
@@ -38,64 +37,73 @@ def get_header(soup):
         return ""
     return header.p.get_text()
 
+
 def parse_header(header):
-    #header could be sometthing like a quote "The best way to predict the future is to invent it. // Alan Kay"
-    #the author is separated by // from the quote
+    # header could be sometthing like a quote "The best way to predict the future is to invent it. // Alan Kay"
+    # the author is separated by // from the quote
     split = header.split("//")
     if len(split) == 2:
         return split[0], split[1]
     else:
         return header, ""
 
+
 class article:
-    def __init__(self, mainurl, title, text, subtext, suburl, category): 
+    def __init__(self, mainurl, title, text, subtext, suburl, category):
         self.mainurl = mainurl
-        self.title = title          
+        self.title = title
         self.text = text
         self.subtext = subtext
         self.suburl = suburl
         self.category = category
+
     def __str__(self):
-        return self.mainurl + "-" + self.title + "-" + self.text + "-" + self.subtext + "-" + self.suburl
+        return (
+            self.mainurl
+            + "-"
+            + self.title
+            + "-"
+            + self.text
+            + "-"
+            + self.subtext
+            + "-"
+            + self.suburl
+        )
+
 
 def get_articles(soup):
-    categories =[]
+    categories = []
     articles = []
     content = soup.find(id="content")
-    #all content starts with a h2
+    # all content starts with a h2
     for h2element in content.find_all("h2"):
         category = h2element.get_text()
         categories.append(category)
-        #print(h2element.get_text())
+        # print(h2element.get_text())
         for nextSibling in h2element.next_siblings:
             if nextSibling.name == "h2":
                 break
             if nextSibling.name == "p":
-                
                 a = nextSibling.find("a")
                 if a is not None:
                     mainurl = a.get("href")
                     title = a.get("title")
                     text = a.get_text()
-                    subtext =""
+                    subtext = ""
                     suburl = ""
-                    span = nextSibling.find("span") 
+                    span = nextSibling.find("span")
                     if span is not None:
                         subtext = span.text
-                        suburla = span.find("a") 
-                        if suburla is not None: 
+                        suburla = span.find("a")
+                        if suburla is not None:
                             suburl = suburla.get("href")
-                    art = article(mainurl, title, text, subtext, suburl, category)    
+                    art = article(mainurl, title, text, subtext, suburl, category)
                     articles.append(art)
-                    print(art)       
+                    print(art)
     return articles, categories
 
 
-
-
-
-
-html = download_html("https://mailchi.mp/hackernewsletter/"+ISSUE)
+html = download_html("https://mailchi.mp/hackernewsletter/" + ISSUE)
 soup = parse_html(html)
 header = get_header(soup)
 
@@ -103,8 +111,6 @@ articles, categories = get_articles(soup)
 
 # parse the content of each link
 newsitems = []
-
-
 
 
 handlers = [YoutubeHandler(), PDFHandler(), GithubHandler(), DefaultHandler()]
@@ -128,11 +134,7 @@ paperdata["quoteLine"] = quoteLine
 paperdata["quoteAuthor"] = quoteAuthor
 
 
-DICT_VALS = {
-    'data' : paperdata,
-    'categories': categories,
-    'newsitems': newsitems
-    }
+DICT_VALS = {"data": paperdata, "categories": categories, "newsitems": newsitems}
 
 # Do the latex stuff
 from latexbuild import render_latex_template
@@ -143,15 +145,12 @@ PATH_OUTPUT_PDF = "MYOUTPUTFILE.pdf"
 
 # Build Jinja2 template, compile result latex, move compiled file to output path,
 # and clean up all intermediate files
-#build_pdf(PATH_JINJA2, PATH_TEMPLATE_RELATIVE_TO_PATH_JINJA2, PATH_OUTPUT_PDF, DICT_VALS)
+# build_pdf(PATH_JINJA2, PATH_TEMPLATE_RELATIVE_TO_PATH_JINJA2, PATH_OUTPUT_PDF, DICT_VALS)
 
 latexresult = render_latex_template(
-    PATH_JINJA2,
-    PATH_TEMPLATE_RELATIVE_TO_PATH_JINJA2,
-    DICT_VALS
-    )
+    PATH_JINJA2, PATH_TEMPLATE_RELATIVE_TO_PATH_JINJA2, DICT_VALS
+)
 
 # store latexresult in a file using utf8 encoding
 with open("output.tex", "w", encoding="utf-8") as f:
     f.write(latexresult)
-    
