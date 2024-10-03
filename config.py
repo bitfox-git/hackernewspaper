d43f7d606773b378e5f504bc639f2473d00d47d4
Sjoerd <sjoerdhoogvliet@gmail.com>
2024-10-03 11:50:42 +0200

    Remove multiple indexing

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 import sys
 
-#read the first argument from the command line and use it as the issue number, second argument is the latest git tag
+#read the first argument from the command line and use it as the issue number
 if len(sys.argv) > 1:
     ISSUE = sys.argv[1]
-    LATEST_GIT_TAG = int(sys.argv[2])
 else:
     ISSUE = "675" 
 
